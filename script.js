80de0a7706ef640cec35c15b146cd0eda100cf37
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-08 10:25:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const hamburger=document.querySelector(".hamburger");
-const navul=document.querySelector(".navul");
-const header=document.querySelector(".heada")
+const hamburger = document.querySelector('.hamburger');
+const navul = document.querySelector('.navul');
+const header = document.querySelector('.heada');
 
-hamburger.addEventListener("click",()=>{
-    hamburger.classList.toggle("active");
-    navul.classList.toggle("active");
-    header.classList.toggle("active");
-})
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navul.classList.toggle('active');
+  header.classList.toggle('active');
+});
 
-document.querySelectorAll(".navitem").forEach(n=>n.addEventListener("click",()=>{
-    hamburger.classList.remove("active");
-    navul.classList.remove("active");
-    header.classList.remove("active");
-}))
+document.querySelectorAll('.navitem').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navul.classList.remove('active');
+  header.classList.remove('active');
+}));
