525cf0f7bfa14dc3524b4d2bac1635c3ac809e22
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-08 11:42:44 +0100

    Added changes requested by the reviewer

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const hamburger = document.querySelector('.hamburger');
 const navul = document.querySelector('.navul');
-const header = document.querySelector('.heada');
+const main = document.querySelector('.main');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navul.classList.toggle('active');
-  header.classList.toggle('active');
+  main.classList.toggle('active');
 });
 
 document.querySelectorAll('.navitem').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navul.classList.remove('active');
-  header.classList.remove('active');
+  main.classList.remove('active');
 }));
