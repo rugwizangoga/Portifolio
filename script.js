dd6d038565b393b1f50f71c9b929ef7fa8bcd532
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-09-10 11:04:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const navul = document.querySelector('.navul');
 const main = document.querySelector('.main');
 const works = document.getElementById('portifolio');
 
-
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active');
   navul.classList.toggle('active');
@@ -16,8 +15,7 @@ document.querySelectorAll('.navitem').forEach((n) => n.addEventListener('click',
   main.classList.remove('active');
 }));
 
-
-let projects = [
+const projects = [
   {
     id: 'proj1',
     title: 'Tonic',
@@ -26,11 +24,11 @@ let projects = [
     year: 2015,
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: {
-      tech1: 'html', 
+      tech1: 'html',
       tech2: 'css',
-      tech3: 'javaScript'
+      tech3: 'javaScript',
     },
-    image: './images/over.png'
+    image: './images/over.png',
   },
 
   {
@@ -41,45 +39,46 @@ let projects = [
     year: 2015,
     description: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     technologies: {
-      tech1: 'html', 
+      tech1: 'html',
       tech2: 'css',
-      tech3: 'javaScript'
+      tech3: 'javaScript',
     },
-    image: './images/newcard.png'
-   },
+    image: './images/newcard.png',
+  },
 
-   {
+  {
     id: 'proj3',
     title: 'Facebook 360',
     company: 'FACEBOOK',
     specialization: 'Full Stack Dev',
     year: 2015,
- 
+
     description: 'Exploring the future of media in Facebook\'s first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
     technologies: {
-      tech1: 'html', 
+      tech1: 'html',
       tech2: 'css',
-      tech3: 'javaScript'
-     },
-    image: './images/backcard.png'
-   },
+      tech3: 'javaScript',
+    },
+    image: './images/backcard.png',
+  },
 
-   {
-    id:'proj4',
+  {
+    id: 'proj4',
     title: 'Uber Navigation',
     company: 'UBER',
     specialization: 'Lead Developer',
     year: 2018,
- 
+
     description: 'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     technologies: {
-      tech1: 'html', 
+      tech1: 'html',
       tech2: 'css',
-      tech3: 'javaScript'
-     },
-    image: './images/last.png'}
+      tech3: 'javaScript',
+    },
+    image: './images/last.png',
+  },
 ];
-let count =1;
+let count = 1;
 projects.forEach((project) => {
   const newdiv = document.createElement('div');
   newdiv.innerHTML = `
@@ -109,11 +108,11 @@ projects.forEach((project) => {
 });
 
 const image = document.querySelectorAll('.image');
-image.forEach((im) =>{
-  if (count%2 === 0){
+image.forEach((im) => {
+  if (count % 2 === 0) {
     im.classList.add('two');
   }
-  count++;
+  count += 1;
 });
 
 const seeproject = document.querySelectorAll('.see');
@@ -122,7 +121,7 @@ const popup = document.querySelector('.popup');
 const over = document.querySelector('.over');
 
 seeproject.forEach((p) => p.addEventListener('click', (p) => {
-  let id = p.target.id;
+  const { id } = p.target;
   const pop = projects.find((p) => p.id === id);
   popup.innerHTML = `
   <div class="titles">
@@ -156,10 +155,10 @@ seeproject.forEach((p) => p.addEventListener('click', (p) => {
             </button>
           </div>
       </div>
-  </div>  `
+  </div>  `;
   overlay.classList.toggle('active');
   popup.classList.toggle('active');
-  over.classList.toggle('active')
+  over.classList.toggle('active');
 
   const close = document.querySelector('.close');
 
